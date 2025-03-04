dbd60a59d16579cba88a85a3d5cab1dc0ecc236f
Gustavo Calastro <gu.calastro@gmail.com>
2025-03-04 19:15:23 -0300

    Mudanças no tamanho. Som de pontos Estilos

--- app.js
+++ app.js
@@ -1,134 +1,175 @@
-
-document.addEventListener('DOMContentLoaded' , () => {
-    const bird = document.querySelector('.bird')
-    const gameDisplay = document.querySelector('.game-container')
-    const ground = document.querySelector('.ground-moving')
+document.addEventListener('DOMContentLoaded', () => {
+    const bird = document.querySelector('.bird');
+    const gameDisplay = document.querySelector('.game-container');
+    const ground = document.querySelector('.ground-moving');
 
     let birdLeft = 220;
     let birdBottom = 150;
     let gravity = 3;
     let isGameOver = false;
-    let gap = 430;
+    let gap = 550;
     let updateCounter = document.getElementById('count');
     let counter = 0;
-
-    function count() {
-    if (counter < 4) {
-    updateCounter.innerHTML = counter;
-    counter++;
-    setTimeout(count, 1000);
-    } else {
-    if (counter === 4) {
-        updateCounter.innerHTML = "Go!";
-    }
-    }
-    window.location.href = 'index.html';
-    startGame();
-}
+    let points = 0;
 
     function startGame() {
-        birdBottom -= gravity
-        bird.style.bottom = birdBottom + 'px'
-        bird.style.left = birdLeft + 'px'
+        birdBottom -= gravity;
+        bird.style.bottom = birdBottom + 'px';
+        bird.style.left = birdLeft + 'px';
     }
-    let gameTimerId = setInterval(startGame, 20)
+
+    let gameTimerId = setInterval(startGame, 20);
+
+    // Function to control the bird jump
+function jump() {
+    if (birdBottom < 500) birdBottom += 50;
+    bird.style.bottom = birdBottom + 'px';
+
+    points += 25;
+    document.getElementById('points').innerHTML = points;
+    document.getElementById("points").style.fontSize = "50px"; // Fonte padrão
+
+    if (points == 1000 ) {
+        triggerFontEffect("red", 70, 'sounds/get-tem.mp3');
+    }
+
+    if (points == 2000) {
+        triggerFontEffect("blue", 80, 'sounds/get-tem.mp3');
+    }
+
+    if (points == 3000) {
+        triggerFontEffect("green", 90, 'sounds/get-tem.mp3');
+    }
+
+    if (points == 4000) {
+        triggerFontEffect("orange", 100, 'sounds/get-tem.mp3');
+    }
+
+    if (points == 5000) {
+        triggerFontEffect("purple", 110, 'sounds/get-tem.mp3');
+    }
+}
+
+// Função para aplicar o efeito de aumentar fonte e cor, além de adicionar som
+function triggerFontEffect(color, size, soundUrl) {
+    // Aumenta o tamanho da fonte e muda a cor por 1 segundo
+    let pointsElement = document.getElementById("points");
+    
+    // Audio de efeito especial
+    const specialSound = new Audio(soundUrl);
+    specialSound.play();
+
+    // Modificando o estilo da fonte
+    pointsElement.style.fontSize = size + "px";
+    pointsElement.style.color = color;
+
+    // Volta ao tamanho original após 1 segundo
+    setTimeout(() => {
+        pointsElement.style.fontSize = "50px"; // Retorna ao tamanho original
+        pointsElement.style.color = "red";  // Retorna à cor original
+    }, 1000);  // 1000ms = 1 segundo
+}
+
+
+    // Listen for keyboard input (Spacebar to jump)
+    document.addEventListener('keyup', control);
 
     function control(e) {
         if (e.keyCode === 32) {
-            jump()
+            jump();
         }
     }
 
-    function jump() {
-        if (birdBottom < 500) birdBottom += 50
-        bird.style.bottom = birdBottom + 'px'
-        console.log(birdBottom)
-    }
-    document.addEventListener('keyup', control)
+    // Add touch event listener for tap to jump
+    document.addEventListener('touchstart', function() {
+        jump(); // Trigger jump on touch
+    });
 
     function generateObstacle() {
-        let obstacleLeft = 500
-        let randomHeight = Math.random() * 60
-        let obstacleBottom = randomHeight
-        const obstacle = document.createElement('div')
-        const topObstacle = document.createElement('div')
+        let obstacleLeft = 2000;
+        let randomHeight = Math.random() * 60;
+        let obstacleBottom = randomHeight;
+        const obstacle = document.createElement('div');
+        const topObstacle = document.createElement('div');
         if (!isGameOver) {
-            obstacle.classList.add('obstacle')
-            topObstacle.classList.add('topObstacle')
+            obstacle.classList.add('obstacle');
+            topObstacle.classList.add('topObstacle');
         }
-        gameDisplay.appendChild(obstacle)
-        gameDisplay.appendChild(topObstacle)
-        obstacle.style.left = obstacleLeft + 'px'
-        topObstacle.style.left = obstacleLeft + 'px'
-        obstacle.style.bottom = obstacleBottom + 'px'
-        topObstacle.style.bottom = obstacleBottom + gap + 'px'
+        gameDisplay.appendChild(obstacle);
+        gameDisplay.appendChild(topObstacle);
+        obstacle.style.left = obstacleLeft + 'px';
+        topObstacle.style.left = obstacleLeft + 'px';
+        obstacle.style.bottom = obstacleBottom + 'px';
+        topObstacle.style.bottom = obstacleBottom + gap + 'px';
 
         function moveObstacle() {
-            obstacleLeft -=2
-            obstacle.style.left = obstacleLeft + 'px'
-            topObstacle.style.left = obstacleLeft + 'px'
+            obstacleLeft -= 10;
+            
+            obstacle.style.left = obstacleLeft + 'px';
+            topObstacle.style.left = obstacleLeft + 'px';
+            console.log("obstacleLeft",obstacleLeft);
+            console.log("obstacleBottom",obstacleBottom);
+            console.log("birdLeft",birdLeft);
+            console.log("birdBottom",birdBottom);
 
-            if (obstacleLeft === -60) {
-                clearInterval(timerId)
-                gameDisplay.removeChild(obstacle)
-                gameDisplay.removeChild(topObstacle)
+            if (obstacleLeft === -100) {
+                console.log('Obstacle removed');
+                clearInterval(timerId);
+                gameDisplay.removeChild(obstacle);
+                gameDisplay.removeChild(topObstacle);
             }
             if (
                 obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
-                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap -200)||
-                birdBottom === 0 
-                ) {
-                gameOver()
-                clearInterval(timerId)
+                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap - 200) ||
+                birdBottom === 0
+                
+            ) {
+                console.log('Game Over');
+                gameOver();
+                clearInterval(timerId);
             }
         }
-        let timerId = setInterval(moveObstacle, 20) 
-        if (!isGameOver) setTimeout(generateObstacle, 3000)
 
+        let timerId = setInterval(moveObstacle, 20);
+        if (!isGameOver) setTimeout(generateObstacle, 3000);
     }
-    generateObstacle()
 
-function gameOver() {
-    clearInterval(gameTimerId);
-    isGameOver = true;
-    document.removeEventListener('keyup', control);
-    ground.classList.add('ground');
-    ground.classList.remove('ground-moving');
+    generateObstacle();
 
-    // Esconder o pássaro
-    const birdElement = document.querySelector('.bird');
-    const birdLeft = birdElement.style.left;
-    const birdBottom = birdElement.style.bottom;
+    function gameOver() {
+        clearInterval(gameTimerId);
+        isGameOver = true;
+        document.removeEventListener('keyup', control);
+        ground.classList.add('ground');
+        ground.classList.remove('ground-moving');
 
-    birdElement.style.display = 'none'; // Esconde o pássaro
+        const birdElement = document.querySelector('.bird');
+        const birdLeft = birdElement.style.left;
+        const birdBottom = birdElement.style.bottom;
 
-    // Criar o GIF de explosão
-    const explosionGif = document.createElement('img');
-    explosionGif.src = 'images/explosion.gif';  // Caminho para o GIF de explosão
-    explosionGif.alt = 'Explosion';
-    explosionGif.classList.add('explosion');  // Adiciona a classe para estilizar
+        birdElement.style.display = 'none';
 
-    // Adicionar o GIF de explosão na mesma posição onde o pássaro estava
-    const skyElement = document.querySelector('.sky');
-    explosionGif.style.position = 'absolute';
-    explosionGif.style.left = birdLeft;  // Mesma posição de left do pássaro
-    explosionGif.style.bottom = birdBottom;  // Mesma posição de bottom do pássaro
+        const explosionGif = document.createElement('img');
+        explosionGif.src = 'images/explosion.gif';
+        explosionGif.alt = 'Explosion';
+        explosionGif.classList.add('explosion');
 
-    skyElement.appendChild(explosionGif);  // Coloca o GIF dentro da .sky
+        const skyElement = document.querySelector('.sky');
+        explosionGif.style.position = 'absolute';
+        explosionGif.style.left = birdLeft;
+        explosionGif.style.bottom = birdBottom;
 
-    // Tocar o som de explosão
-    const explosionSound = new Audio('sounds/explosion.mp3');
-    explosionSound.play();
+        skyElement.appendChild(explosionGif);
 
-    // Remover a explosão após 3 segundos para garantir tempo de visualização
-    setTimeout(() => {
-        explosionGif.remove(); // Remove a explosão após 3 segundos
-    }, 3000);  // Ajuste o tempo para garantir que a explosão seja visível
+        const explosionSound = new Audio('sounds/explosion.mp3');
+        explosionSound.play();
 
-    // Redirecionar após 3 segundos (para dar tempo ao efeito de explosão)
-    setTimeout(() => {
-        window.location.href = 'deathScreen.html'; // Redireciona após o tempo da explosão
-    }, 3000);  // Ajuste o tempo de redirecionamento
-}
+        setTimeout(() => {
+            explosionGif.remove();
+        }, 3000);
 
-})
+        setTimeout(() => {
+            window.location.href = 'deathScreen.html';
+        }, 20000000);
+    }
+});
